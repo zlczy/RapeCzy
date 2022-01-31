bf24a7a3202d618fcbb62643d2117552c0cb4e7e
zlczy <98568854+zlczy@users.noreply.github.com>
2022-01-31 16:53:26 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,11 +348,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '逊呐';
-        if (score <= 99) return '哼哼哼';
-        if (score <= 149) return '你是一个一个一个';
-        if (score <= 199) return '哼哼哼啊啊啊啊啊';
-        return '都撅烂了罢（恼';
+        if (score <= 49) return '太蔡了';
+        if (score <= 99) return '太蓝了';
+        if (score <= 149) return '你是一个一个一个小狗逼';
+        if (score <= 199) return 'Czy都撅烂了罢（恼';
+        return '压力马斯内';
     }
 
     w.toStr = function(obj) {
